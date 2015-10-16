7dc20ac82ca1d16e0b584e5200c491be54f9b7df
Hlynur Halldórsson <hlynurhalldorsson92@gmail.com>
2015-10-16 18:46:24 +0000

    TestCase for unkown number of numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,7 +2,18 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 
-	public static int add(String text){
- 		return 0;
+	public static int add(String text) {
+		if(text.equals("")) {
+			return 0;
+		}
+		else if(text.contains(",")) {
+			String [] numbers = text.split(",");
+			return toInt(numbers[0]) + toInt(numbers[1]);
+		}
+		else return 1;
+	}
+
+	private static int toInt(String number) {
+		return Integer.parseInt(number);
 	}
 } 
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -10,8 +10,26 @@ public class CalculatorTest {
     }
 
 	@Test
-	public void testEpmtyString() {
+	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}	
-	
-}
+
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
+	}	
+
+	@Test
+	public void testTwoNumbers() {
+		assertEquals(3, Calculator.add("1,2"));
+		assertEquals(0, Calculator.add("0,0"));
+		assertEquals(5, Calculator.add("2,3"));
+	}	
+
+	@Test
+	public void testUnkownNumberOfNumbers(){
+		assertEquals(0, Calculator.add("0,0,0"));
+		assertEquals(3, Calculator.add("1,1,1"));
+		assertEquals(10, Calculator.add("1,2,3,4"));
+	}
+} 
