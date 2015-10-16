20cfad0b6402d0b1b9fc26cf8776a2dbcbb92c50
Hlynur Halldórsson <hlynurhalldorsson92@gmail.com>
2015-10-16 20:36:17 +0000

    Added functionality to add function, now it can read unkown number of numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,17 +3,48 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text) {
+		
 		if(text.equals("")) {
 			return 0;
 		}
-		else if(text.contains(",")) {
+		if(text.length() == 1) {
+			return toInt(text);
+		}	
+
+		if(text.length() == 3) {
 			String [] numbers = text.split(",");
 			return toInt(numbers[0]) + toInt(numbers[1]);
 		}
+			//If the string holds more than 3 chars
+			//then there are 3 or more numbers 
+		if(text.length() > 3) {
+			
+			String [] numbers = text.split(",");
+			int total = 0;
+	
+			for(int i = 0; i < numbers.length; i++)
+			{
+				total += toInt(numbers[i]);
+			}
+			return total;
+		}
+		
 		else return 1;
 	}
 
 	private static int toInt(String number) {
 		return Integer.parseInt(number);
 	}
+
+	// private static int unknownNumberCount(String numbers){
+		
+	// 	int total = 0;
+
+	// 	for(int i = 0; i < numbers.length(); i++)
+	// 	{
+	// 		total += toInt(numbers[i]);
+	// 	}
+	// 	return total;
+	// }
+
 } 
